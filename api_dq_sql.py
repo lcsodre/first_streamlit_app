4b757c6295d08d55013fe9e3e33b8a0538046ffe
lcsodre <131267576+lcsodre@users.noreply.github.com>
2023-04-26 17:39:50 -0300

    Update api_dq_sql.py

--- api_dq_sql.py
+++ api_dq_sql.py
@@ -54,7 +54,7 @@ openai.api_key = streamlit.secrets['pass']
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
+  prompt="### Postgres SQL tables, with their properties: Employee(id, name, department_id) Department(id, name, address) Salary_Payments(id, employee_id, amount, date) A query to list the names of the departments which employed more than 10 employees in the last 3 months SELECT",
   temperature=0,
   max_tokens=150,
   top_p=1.0,
