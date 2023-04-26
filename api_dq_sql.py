44509e5edcf23d9a8b5cc840a35c72382ec639a0
lcsodre <131267576+lcsodre@users.noreply.github.com>
2023-04-26 17:49:29 -0300

    Update api_dq_sql.py

--- api_dq_sql.py
+++ api_dq_sql.py
@@ -47,7 +47,7 @@ if streamlit.button('Get Columns'):
   p_column = streamlit.selectbox('Tables',my_data_rows)
 
 
-b_rule = streamlit.text_area('Busines rule', height=300)
+b_rule = streamlit.text_area('Busines rule', height=300,'#Snowflake \n'+p_structure+'('+p_column+')')
 
 if streamlit.button('Gather SQL'):
  
