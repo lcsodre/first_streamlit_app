ecf728619c777ac3a0bc3fda4160ce201613bee3
lcsodre <131267576+lcsodre@users.noreply.github.com>
2023-04-27 16:38:04 -0300

    Update dqfmw_structures.py

--- dqfmw_structures.py
+++ dqfmw_structures.py
@@ -8,7 +8,7 @@ from urllib.error import URLError
 def get_databases_load_list():
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select DATABASE_NAME FROM SNOWFLAKE.INFORMATION_SCHEMA.DATABASES")
+    my_cur.execute("select DATABASE_NAME FROM SNOWFLAKE.INFORMATION_SCHEMA.DATABASES order by DATABASE_NAME")
     f_return=my_cur.fetchall() 
     my_cnx.close()
     df = pd.DataFrame(f_return,columns=['Name'])
@@ -17,7 +17,7 @@ def get_databases_load_list():
 def get_tables_load_list(p_catalog):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
-    v_query="SELECT TABLE_SCHEMA || '.' || TABLE_NAME AS NAME FROM " +p_catalog+ ".INFORMATION_SCHEMA.TABLES"
+    v_query="SELECT TABLE_SCHEMA || '.' || TABLE_NAME AS NAME FROM " +p_catalog+ ".INFORMATION_SCHEMA.TABLES order by TABLE_SCHEMA,TABLE_NAME"
     my_cur.execute(v_query)
     f_return=my_cur.fetchall() 
     my_cnx.close()
@@ -27,7 +27,7 @@ def get_tables_load_list(p_catalog):
 def get_domains_load_list():
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
-    v_query="SELECT DOMAIN_ID || '|' || DOMAIN_NAME FROM DMDQFMRWK.METADATA.DOMAINS"
+    v_query="SELECT DOMAIN_ID || '|' || DOMAIN_NAME FROM DMDQFMRWK.METADATA.DOMAINS order by DOMAIN_NAME"
     my_cur.execute(v_query)
     f_return=my_cur.fetchall() 
     my_cnx.close()
@@ -37,7 +37,7 @@ def get_domains_load_list():
 def get_responsibles_load_list():
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
-    v_query="SELECT RESPONSIBLE_ID || '|' || RESPONSIBLE_NAME FROM DMDQFMRWK.METADATA.RESPONSIBLES"
+    v_query="SELECT RESPONSIBLE_ID || '|' || RESPONSIBLE_NAME FROM DMDQFMRWK.METADATA.RESPONSIBLES order by RESPONSIBLE_NAME"
     my_cur.execute(v_query)
     f_return=my_cur.fetchall() 
     my_cnx.close()
@@ -54,7 +54,7 @@ def insert_row_snowflake(p_custodian_id,p_steward_id,p_owner_id,p_domain_id,p_st
 def get_structures_load_list():
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
-    v_query="SELECT STRUCTURE_ID || '|' || STRUCTURE_NAME FROM DMDQFMRWK.METADATA.STRUCTURES"
+    v_query="SELECT STRUCTURE_ID || '|' || STRUCTURE_NAME FROM DMDQFMRWK.METADATA.STRUCTURES order by STRUCTURE_NAME "
     my_cur.execute(v_query)
     f_return=my_cur.fetchall() 
     my_cnx.close()
