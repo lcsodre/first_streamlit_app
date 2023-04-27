cbd9d0b83cd9fff95618fa3762894f4a3e101103
lcsodre <131267576+lcsodre@users.noreply.github.com>
2023-04-27 16:29:19 -0300

    Update dqfmw_structures.py

--- dqfmw_structures.py
+++ dqfmw_structures.py
@@ -67,61 +67,60 @@ streamlit.title('DQ - Structures')
 streamlit.sidebar.title('Menu')
 page_domains = streamlit.sidebar.selectbox('Subject',['Domains','Responsible','Dimensions','Structures','Rules'])
 
-with streamlit.form(key="struct_ins"):
-  my_data_rows = get_databases_load_list()
-  p_database = streamlit.selectbox('Database',my_data_rows)
-  
-  my_data_rows = get_tables_load_list(p_database)
-  p_table = streamlit.selectbox('Table',my_data_rows)
-  
-  p_structure = p_database+"."+p_table
-  
-  my_data_rows = get_domains_load_list()
-  p_domains = streamlit.selectbox('Domain',my_data_rows)
-  
-  #Retrieve ID
-  p_dom_split_id = p_domains.split('|')
-  p_dom_id = p_dom_split_id[0]
-  #Retrive Name
-  p_dom = p_dom_split_id[1]
-  
-  my_data_rows = get_responsibles_load_list()
-  p_custodian = streamlit.selectbox('Custodian',my_data_rows)
-  
-  #Retrieve ID
-  p_cust_split_id = p_custodian.split('|')
-  p_cust_id = p_cust_split_id[0]
-  #Retrive Name
-  p_cust = p_cust_split_id[1]
-  
-  my_data_rows = get_responsibles_load_list()
-  p_steward = streamlit.selectbox('Steward',my_data_rows)
-  
-  #Retrieve ID
-  p_stew_split_id = p_steward.split('|')
-  p_stew_id = p_stew_split_id[0]
-  #Retrive Name
-  p_stew = p_stew_split_id[1]
-  
-  my_data_rows = get_responsibles_load_list()
-  p_owner = streamlit.selectbox('Owner',my_data_rows)
-  
-  #Retrieve ID
-  p_owner_split_id = p_owner.split('|')
-  p_owner_id = p_owner_split_id[0]
-  #Retrive Name
-  p_owner = p_owner_split_id[1]
-  
-  p_structure_desc = streamlit.text_area('Structure Description', height=100)
+my_data_rows = get_databases_load_list()
+p_database = streamlit.selectbox('Database',my_data_rows)
 
-  my_data_rows = get_structures_load_list()
-  df = pd.DataFrame(my_data_rows,columns=['Name'])
-  streamlit.table(df)
-  
-  input_button = streamlit.form_submit_button('Add Structure')
-  
-  #Button to insert into Snowflake
-  if input_button:
-    message_insert = insert_row_snowflake(p_cust_id,p_stew_id,p_owner_id,p_dom_id,p_structure,p_structure_desc)
-    streamlit.text(message_insert)
- 
+my_data_rows = get_tables_load_list(p_database)
+p_table = streamlit.selectbox('Table',my_data_rows)
+
+p_structure = p_database+"."+p_table
+
+my_data_rows = get_domains_load_list()
+p_domains = streamlit.selectbox('Domain',my_data_rows)
+
+#Retrieve ID
+p_dom_split_id = p_domains.split('|')
+p_dom_id = p_dom_split_id[0]
+#Retrive Name
+p_dom = p_dom_split_id[1]
+
+my_data_rows = get_responsibles_load_list()
+p_custodian = streamlit.selectbox('Custodian',my_data_rows)
+
+#Retrieve ID
+p_cust_split_id = p_custodian.split('|')
+p_cust_id = p_cust_split_id[0]
+#Retrive Name
+p_cust = p_cust_split_id[1]
+
+my_data_rows = get_responsibles_load_list()
+p_steward = streamlit.selectbox('Steward',my_data_rows)
+
+#Retrieve ID
+p_stew_split_id = p_steward.split('|')
+p_stew_id = p_stew_split_id[0]
+#Retrive Name
+p_stew = p_stew_split_id[1]
+
+my_data_rows = get_responsibles_load_list()
+p_owner = streamlit.selectbox('Owner',my_data_rows)
+
+#Retrieve ID
+p_owner_split_id = p_owner.split('|')
+p_owner_id = p_owner_split_id[0]
+#Retrive Name
+p_owner = p_owner_split_id[1]
+
+p_structure_desc = streamlit.text_area('Structure Description', height=100)
+
+my_data_rows = get_structures_load_list()
+df = pd.DataFrame(my_data_rows,columns=['Name'])
+streamlit.table(df)
+
+input_button = streamlit.form_submit_button('Add Structure')
+
+#Button to insert into Snowflake
+if input_button:
+  message_insert = insert_row_snowflake(p_cust_id,p_stew_id,p_owner_id,p_dom_id,p_structure,p_structure_desc)
+  streamlit.text(message_insert)
+
