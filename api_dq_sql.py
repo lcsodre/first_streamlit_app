f42b2a53aa888054862398e85c248322b7a99903
lcsodre <131267576+lcsodre@users.noreply.github.com>
2023-05-08 13:10:11 -0300

    Update api_dq_sql.py

--- api_dq_sql.py
+++ api_dq_sql.py
@@ -92,8 +92,6 @@ p_table=str(p_table)
 my_data_rows = get_attributes_list(p_catalog,p_schema,p_table)
 p_column = streamlit.selectbox('Columns',my_data_rows)
 
-time.sleep(5) 
-  
 my_data_rows = get_dimensions_list()
 p_dim = streamlit.selectbox('Dimensions',my_data_rows)
 
